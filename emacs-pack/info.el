2ccf907ce790a88c3478bbaa69c317b995d13b95
Andrea Richiardi <a.richiardi.work@gmail.com>
2026-07-02 15:44:41 -0600

    Update the info file and bump version

--- emacs-pack/info.el
+++ emacs-pack/info.el
@@ -1,3 +1,13 @@
+;;; info.el --- Info file for the ar-org-pack  -*- lexical-binding: t; -*-
+;;
+;; Author: Andrea Richiardi
+
+;;; Commentary:
+;;
+;;; Code:
+
 (live-pack-name "ar-org-pack")
-(live-pack-version "0.0.1")
-(live-pack-description "A pack for org documents.")
+(live-pack-version "0.0.2")
+(live-pack-description "A pack for my org documents.")
+
+;;; info.el ends here
